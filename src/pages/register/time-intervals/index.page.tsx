8e69125d800407f65a8d5e7b0adf7a6d8ce4022e
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-11-11 23:00:53 -0300

    feat: add time intervals db scheme and post implementation

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -22,6 +22,7 @@ import { getWeekDays } from '@/utils/get-week-days'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Container, Header } from '../styles'
 import { convertTimeStringToMinutes } from '@/utils/convert-time-string-to-minutes'
+import { api } from '@/lib/axios'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -96,7 +97,9 @@ export default function TimeIntervals() {
   const intervals = watch(`intervals`)
 
   async function handleSetTimeIntervals(data: TimeIntervalsFormOutput) {
-    console.log(data)
+    const { intervals } = data as TimeIntervalsFormOutput
+
+    await api.post('/users/time-intervals', intervals)
   }
 
   return (
