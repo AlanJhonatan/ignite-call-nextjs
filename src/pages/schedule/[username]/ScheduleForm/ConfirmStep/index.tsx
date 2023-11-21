83f8b6b0c51e77a1e0cf4189ea4b9f600d540aa9
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-11-20 22:50:32 -0300

    feat: add confirmstep form

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,3 +1,46 @@
+import { Button, Text, TextArea, TextInput } from '@zizao/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+
 export function ConfirmStep() {
-  return <div>Confirm Step</div>
+  function handleFormSubmit() {
+    return null
+  }
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleFormSubmit}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          20 de novembro de 2023
+        </Text>
+        <Text>
+          <Clock />
+          13:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
